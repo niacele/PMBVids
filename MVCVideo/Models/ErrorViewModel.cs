1abd848c313282cc5552bc08a50c5ba7074ffd44
lab_services_student <lab_services_student@lab7L95SR>
2025-02-21 15:39:49 +0200

    Add project files.

---
+++ MVCVideo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCVideo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
