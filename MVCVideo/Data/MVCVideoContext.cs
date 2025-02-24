8300b295acb19a91bb0dbc391b4ab90446c66a2a
lab_services_student <lab_services_student@lab7L95SR>
2025-02-24 16:06:39 +0200

    added a model

---
+++ MVCVideo/Data/MVCVideoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MVCVideo.Data
+{
+    public class MVCVideoContext : DbContext
+    {
+        public MVCVideoContext (DbContextOptions<MVCVideoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- MVCVideo/Program.cs
+++ MVCVideo/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCVideo.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MVCVideoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MVCVideoContext") ?? throw new InvalidOperationException("Connection string 'MVCVideoContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
